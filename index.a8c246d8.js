bfcd348eb170ddf201ddca8b386f10a72f9f02d5
KornieievaAnna <109692085+KornieievaAnna@users.noreply.github.com>
2022-10-10 22:01:13 +0000

    Deploying to gh-pages from @ KornieievaAnna/project-01_ice-cream@6b8b1c50b63406b2c1dc6627a8212da07f1d3b16 🚀

---
+++ index.a8c246d8.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.a8c246d8.js.map
