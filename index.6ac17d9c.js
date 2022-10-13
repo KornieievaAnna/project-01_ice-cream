a30424e0e969ae97df860793874313c8ba523215
KornieievaAnna <109692085+KornieievaAnna@users.noreply.github.com>
2022-10-13 16:20:38 +0000

    Deploying to gh-pages from @ KornieievaAnna/project-01_ice-cream@5f06eb24bffa2565d1bc774ce8d10507e5d489d2 🚀

---
+++ index.6ac17d9c.js
@@ -0,0 +1,2 @@
+let o=document.querySelector(".scroll-btn");window.onscroll=function(){window.pageYOffset>200?o.classList.add("shown"):o.classList.remove("shown")},o.onclick=function(){window.scrollTo(0,0)};
+//# sourceMappingURL=index.6ac17d9c.js.map
---
+++ index.80aa350c.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".scroll-btn");window.onscroll=function(){window.pageYOffset>200?o.classList.add("shown"):o.classList.remove("shown")},o.onclick=function(){window.scrollTo(0,0)}}();
+//# sourceMappingURL=index.80aa350c.js.map
